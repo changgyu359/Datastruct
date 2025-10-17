daa2a35e8fc796296dd007cd71ac3568f0c4f893
changgyu359 <gchl8548@gmail.com>
2025-10-17 19:45:47 +0900

    (2025.10.17) 원형 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -24,6 +24,109 @@ public:
         tail = nullptr;
 
     }
+    void push_front(T data)
+    {
+        Node* newNode=new Node;
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+
+        }
+        else
+        {
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
+
+        }
+        size++;
+    }
+    void pop_front()
+    {
+        if (size <= 0)
+        {
+            std::cout << "Link is empty." << std::endl;
+        }
+        else 
+        {
+            Node* deleteNode = head;
+            if(head==tail)
+            {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else
+            {
+                deleteNode->next->prev = nullptr;
+
+                head = head->next;
+            }
+            delete deleteNode;
+            size--;
+        }
+        
+
+    }
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+        if (tail == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+
+        }
+        size++;
+    }
+    void pop_back()
+    {
+        if (size <= 0)
+        {
+            std::cout << "List is empty." << std::endl;
+        }
+        else
+        {
+            Node* deleteNode = tail;
+            if (head == tail)
+            {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else
+            {
+                deleteNode->prev->next = nullptr;
+                tail = tail->prev;
+            }
+            delete deleteNode;
+            size--;
+        }
+        
+        
+    }
+    const bool empty()
+    {
+        return (head == nullptr);
+    }
+    ~List()
+    {
+        while (tail != nullptr)
+        {
+            pop_front();
+        }
+    }
 };
 
 
@@ -34,5 +137,18 @@ int main()
 {
 
     List<int> List;
+
+    List.push_front(10);
+    List.push_front(5);
+
+    List.push_back(20);
+    List.push_back(30);
+
+
+    List.pop_back();
+    List.pop_back(); 
+    List.pop_back();
+    List.pop_back();
+    List.pop_back();
     return 0;
 }
