abd5272bd544e0f460ef190717ded4f86da58132
changgyu359 <gchl8548@gmail.com>
2025-10-24 19:46:06 +0900

    (2025.10.24)우선순위큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,22 +1,20 @@
 ﻿#include <iostream>
-#define SIZE 5
+#define SIZE 4
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 class Queue
 {
 private:
 	int rear;
 	int front;
 	T container[SIZE];
-
 public:
 	Queue()
 	{
-		rear = 0;
-		front = 0;
-
+		rear = SIZE-1;
+		front = SIZE - 1;
 		for (int i = 0; i < SIZE; i++)
 		{
 			container[i] = NULL;
@@ -24,24 +22,65 @@ public:
 	}
 	void push(T data)
 	{
-		if (frpmt == 0 && rear == 5)
+		if ((rear + 1) % SIZE == front)
 		{
 			cout << "Queue overflow" << endl;
 		}
 		else
 		{
-			for (int i = front; i < rear; i++)
-			{
-				container[i - 1] = container[i];
-			}
+			rear = (rear + 1) % SIZE;
+			container[rear] = data;
+		} 
+	}
+	void pop()
+	{
+		if (empty())
+		{
+			cout << "Queue is empty" << endl;
+		}
+		else
+		{
+			front = (front + 1) % SIZE;
+			container[front] = NULL;
+		}
+	}
+
+	const bool& empty()
+	{
+		return front == rear;
+	}
+
+	const T& peek()
+	{
+		if (empty())
+		{
+			exit(1);
+		}
+		else
+		{
+			return container[(front + 1) % SIZE];
 		}
 	}
 };
 
 int main()
 {
-  
 	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+
+	while(queue.empty() == false)
+	{
+		cout << queue.peek() << endl;
+		queue.pop();
+	}
+	
+	queue.push(40);
+	queue.push(50);
+	queue.push(60);
+	
 
 	return 0;
 }
